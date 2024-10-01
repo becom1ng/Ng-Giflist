9a7499e343051e9c2037a4cc43380b68c0eb3cc4
Ryan Lavery <ryan42fbd@gmail.com>
2024-10-01 13:01:30 -0500

    Add and implement infinite scrolling.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { RedditService } from '../shared/data-access/reddit.service';
 import { GifListComponent } from './ui/gif-list.component';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [GifListComponent],
+  imports: [GifListComponent, InfiniteScrollDirective],
   template: `
     <h2>Reddit Gif Player</h2>
-    <app-gif-list
-      [gifs]="redditService.gifs()"
-      class="grid-container"
-    ></app-gif-list>
+    <details>
+      <summary>Expand Gif List</summary>
+      <app-gif-list
+        [gifs]="redditService.gifs()"
+        infiniteScroll
+        (scrolled)="
+          redditService.pagination$.next(redditService.lastKnownGif())
+        "
+        class="grid-container"
+      />
+    </details>
   `,
   styles: ``,
 })
