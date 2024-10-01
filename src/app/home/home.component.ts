36a84a9717a81eb2613ded1991218144cee814d0
Ryan Lavery <ryan42fbd@gmail.com>
2024-10-01 10:11:53 -0500

    Added interface, service, basic gif title list.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RedditService } from '../shared/data-access/reddit.service';
+import { GifListComponent } from './ui/gif-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  template: ` <p>Hello world!</p> `,
+  imports: [GifListComponent],
+  template: `
+    <p>Hello world!</p>
+    <app-gif-list
+      [gifs]="redditService.gifs()"
+      class="grid-container"
+    ></app-gif-list>
+  `,
   styles: ``,
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  redditService = inject(RedditService);
+}
